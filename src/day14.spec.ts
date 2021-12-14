a05dc2d20392c7c8cd7dae3ac31d1ff352085582
John Perrin <john.d.perrin@gmail.com>
2021-12-14 07:05:47 +0000

    day 14 part 1

--- src/day14.spec.ts
+++ src/day14.spec.ts
@@ -1,16 +1,70 @@
-import {parseData} from "./day14";
+import {getAnswer, getElementCounts, parseData, runStep} from "./day14";
 
-const testData = ``.split('\n');
+const testData = `NN
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`.split('\n');
 
 
-test('It parses', ()=>{
-    const data = parseData(testData)
+test('It parses', () => {
+    const [elements, rules] = parseData(testData)
+
+    expect(elements).toBe("NNCB");
+
+    expect(rules.get("CH")).toBe("B");
+    expect(rules.size).toBe(16)
+    expect(rules.get("CN")).toBe("C");
 })
 
-test('part one works', ()=>{
+test('exploring', ()=>{
+    const [elements, rules] = parseData(testData)
+
+    let elementTemp = elements
+    for (let i =0; i<40; i++){
+        elementTemp = runStep(elementTemp, rules)
+        console.log(i, elementTemp.length)
+    }
 
 })
+test('part one works', () => {
+    const [elements, rules] = parseData(testData)
 
-test('part two works', ()=>{
+    const step1 = runStep(elements, rules)
+    expect(step1).toBe("NCNBCHB")
+
+    const step2 = runStep(step1, rules)
+    expect(step2).toBe("NBCCNBBBCBHCB")
+
+    const step3 = runStep(step2, rules)
+    expect(step3).toBe("NBBBCNCCNBBNBNBBCHBHHBCHB")
+
+    const step4 = runStep(step3, rules)
+    expect(step4).toBe("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")
+
+    let elementTemp = elements
+    for (let i =0; i<10; i++){
+        elementTemp = runStep(elementTemp, rules)
+    }
+
+    const counts = getElementCounts(elementTemp)
+
+    expect(getAnswer(counts)).toBe(1588)
+})
+
+test('part two works', () => {
 
 })
--- src/day14.ts
+++ src/day14.ts
@@ -1,15 +1,112 @@
-import {getData, puzzleReturn} from "./common";
+import {puzzleReturn} from "./common";
 
-export function parseData(input: string[]) : unknown {
-    return ""
+type InsertionMap = Map<string, string>
+
+export function parseData(input: string[]): [string, InsertionMap] {
+    let elements = "";
+    let rules: InsertionMap = new Map<string, string>();
+
+    let onRules = false
+
+    for (const line of input) {
+        if (line === "") {
+            onRules = true
+            continue
+        }
+
+        if (onRules) {
+            const bits = line.split(' -> ');
+
+            rules.set(bits[0], bits[1])
+        } else {
+            elements = line
+        }
+    }
+
+    return [elements, rules]
 }
 
-export function PartOne(rawData: string[]) : puzzleReturn {
-    const data = parseData(rawData);
-    return "Not implemented yet"
+export function runStep(element: string, rules: InsertionMap): string {
+    let newElement = "";
+
+    for (let i = 1; i < element.length; i++) {
+        const pair = element[i - 1] + element[i]
+
+        const rule = rules.get(pair)
+
+        if (rule === undefined) {
+            continue
+        }
+
+        newElement += element[i - 1] + rule
+    }
+
+    newElement += element[element.length - 1]
+
+    return newElement
 }
 
-export function PartTwo(rawData: string[]) : puzzleReturn {
-    const data = parseData(rawData);
-    return "Not implemented yet"
+export function getElementCounts(input: string): Map<string, number> {
+    const counts = new Map<string, number>();
+
+    for (const char of input) {
+        const val = counts.get(char)
+
+        if (val === undefined) {
+            counts.set(char, 1)
+        } else {
+            counts.set(char, val + 1)
+        }
+    }
+    return counts
+}
+
+export function getAnswer(counts: Map<string, number>): number {
+
+    let [min, max] = [0,0]
+    let firstval = true
+
+    for (const [k, v] of counts) {
+        if(firstval === true) {
+            min = v
+            max = v
+            firstval = false
+        }
+
+        if (v > max) {
+            max = v
+        }
+
+        if (v < min) {
+            min = v
+        }
+    }
+
+    return max - min
+}
+
+export function PartOne(rawData: string[]): puzzleReturn {
+    const [elements, rules] = parseData(rawData)
+
+    let elementTemp = elements
+    for (let i =0; i<10; i++){
+        elementTemp = runStep(elementTemp, rules)
+    }
+
+    const counts = getElementCounts(elementTemp)
+
+    return getAnswer(counts)
+}
+
+export function PartTwo(rawData: string[]): puzzleReturn {
+    const [elements, rules] = parseData(rawData)
+
+    let elementTemp = elements
+    for (let i =0; i<10; i++){
+        elementTemp = runStep(elementTemp, rules)
+    }
+
+    const counts = getElementCounts(elementTemp)
+
+    return getAnswer(counts)
 }
