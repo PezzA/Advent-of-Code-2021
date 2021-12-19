27d49f4b14cf4e8137b1a0dfc03919a86aab3451
John Perrin <john.d.perrin@gmail.com>
2021-12-19 15:15:18 +0000

    day 14 complete, laternfish approach

--- src/day14.spec.ts
+++ src/day14.spec.ts
@@ -1,4 +1,4 @@
-import {getAnswer, getElementCounts, parseData, runStep} from "./day14";
+import {getAnswer, getElementCounts, InsertionMap, parseData, runStep} from "./day14";
 
 const testData = `NNCB
 
@@ -30,11 +30,11 @@ test('It parses', () => {
     expect(rules.get("CN")).toBe("C");
 })
 
-test('exploring', ()=>{
+test('exploring', () => {
     const [elements, rules] = parseData(testData)
 
     let elementTemp = elements
-    for (let i =0; i<40; i++){
+    for (let i = 0; i < 40; i++) {
         elementTemp = runStep(elementTemp, rules)
         console.log(i, elementTemp.length)
     }
@@ -56,7 +56,7 @@ test('part one works', () => {
     expect(step4).toBe("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")
 
     let elementTemp = elements
-    for (let i =0; i<10; i++){
+    for (let i = 0; i < 10; i++) {
         elementTemp = runStep(elementTemp, rules)
     }
 
@@ -65,6 +65,71 @@ test('part one works', () => {
     expect(getAnswer(counts)).toBe(1588)
 })
 
+
+
 test('part two works', () => {
+    const [elements, rules] = parseData(testData)
+
+    const repMap = getCreates(rules)
+
+    let pairMap = new Map<string, number>();
+
+    for (const [k, v] of repMap) {
+        pairMap.set(k, 0)
+        pairMap.set(v[0], 0)
+        pairMap.set(v[1], 0)
+    }
+
+    for (let i = 1; i < elements.length; i++) {
+        const pair = elements.substring(i - 1, i + 1)
+        const chem = pairMap.get(pair)
+        if (chem === undefined) {
+            pairMap.set(pair, 1)
+        } else {
+            pairMap.set(pair, chem + 1)
+        }
+    }
+
+    for (let i = 0; i < 10; i++) {
+        pairMap = runStep2(pairMap, repMap)
+
+        let total = 0
+        for (const [k, v] of pairMap) {
+            total += v
+        }
+
+    }
+
+    const counts = new Map<string, number>();
+
+    for(const [k, v] of pairMap){
+
+        const amount = v/2
+        const first = k[0]
+        const second = k[1]
+
+        const firstCount = counts.get(first)
+
+        if(firstCount!== undefined){
+            counts.set(first , firstCount+amount)
+        } else{
+            counts.set(first, amount)
+        }
+
+        const secondCount = counts.get(second)
+        if(secondCount!== undefined){
+            counts.set(second , secondCount+amount)
+        } else{
+            counts.set(second, amount)
+        }
+    }
+
+    for(const [k, v] of counts){
+        counts.set(k, Math.ceil(v))
+    }
+
+    console.log(getAnswer(counts))
+
 
 })
+
--- src/day14.ts
+++ src/day14.ts
@@ -1,6 +1,6 @@
 import {Puzzle, PuzzleReturn} from "./common";
 
-type InsertionMap = Map<string, string>
+export type InsertionMap = Map<string, string>
 
 export function parseData(input: string[]): [string, InsertionMap] {
     let elements = "";
@@ -44,6 +44,59 @@ export function runStep(element: string, rules: InsertionMap): string {
     newElement += element[element.length - 1]
 
     return newElement
+}
+
+function cloneMap<T, V>(input: Map<T, V>): Map<T, V> {
+    const map = new Map<T, V>();
+
+    for (const [k, v] of input) {
+        map.set(k, v)
+    }
+    return map
+}
+
+
+export function runStep2(pairMap: Map<string, number>, repMap: Map<string, string[]>): Map<string, number> {
+    const clonedMap = cloneMap(pairMap)
+
+    // for all current pairs
+    for (const [k, v] of clonedMap) {
+
+        if (v === 0) {
+            continue
+        }
+        //get the replacements for the pairs
+        const spawnedPairs = repMap.get(k)
+
+        if (spawnedPairs === undefined) {
+            continue
+        }
+
+        const replacement1 = spawnedPairs[0]
+        const replacement2 = spawnedPairs[1]
+
+        let firstPairCount = pairMap.get(replacement1) ?? 0
+        let secondPairCount = pairMap.get(replacement2) ?? 0
+
+        // in the pair map, will lose the original pairs
+        const currentVal = pairMap.get(k) ?? 0
+
+        pairMap.set(k, currentVal - v)
+
+
+        if (k !== replacement1) {
+            pairMap.set(replacement1, firstPairCount + v)
+        } else {
+            pairMap.set(k, currentVal)
+        }
+
+        if (k !== replacement2) {
+            pairMap.set(replacement2, secondPairCount + v)
+        } else {
+            pairMap.set(k, currentVal)
+        }
+    }
+    return pairMap
 }
 
 export function getElementCounts(input: string): Map<string, number> {
@@ -98,15 +151,86 @@ export function partOne(rawData: string[]): PuzzleReturn {
     return getAnswer(counts)
 }
 
+function getCreates(rules: InsertionMap): Map<string, string[]> {
+    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const replacements = new Map<string, string[]>();
+
+    for (const i of alpha) {
+        for (const j of alpha) {
+            const test = rules.get(i + j)
+
+            if (test === undefined) {
+                continue
+            }
+
+            replacements.set(i + j, [i + test, test + j])
+        }
+    }
+
+    return replacements;
+}
+
+
 export function partTwo(rawData: string[]): PuzzleReturn {
     const [elements, rules] = parseData(rawData)
 
-    let elementTemp = elements
-    for (let i =0; i<10; i++){
-        elementTemp = runStep(elementTemp, rules)
+    const repMap = getCreates(rules)
+
+    let pairMap = new Map<string, number>();
+
+    for (const [k, v] of repMap) {
+        pairMap.set(k, 0)
+        pairMap.set(v[0], 0)
+        pairMap.set(v[1], 0)
     }
 
-    const counts = getElementCounts(elementTemp)
+    for (let i = 1; i < elements.length; i++) {
+        const pair = elements.substring(i - 1, i + 1)
+        const chem = pairMap.get(pair)
+        if (chem === undefined) {
+            pairMap.set(pair, 1)
+        } else {
+            pairMap.set(pair, chem + 1)
+        }
+    }
+
+    for (let i = 0; i < 40; i++) {
+        pairMap = runStep2(pairMap, repMap)
+
+        let total = 0
+        for (const [k, v] of pairMap) {
+            total += v
+        }
+
+    }
+
+    const counts = new Map<string, number>();
+
+    for(const [k, v] of pairMap){
+
+        const amount = v/2
+        const first = k[0]
+        const second = k[1]
+
+        const firstCount = counts.get(first)
+
+        if(firstCount!== undefined){
+            counts.set(first , firstCount+amount)
+        } else{
+            counts.set(first, amount)
+        }
+
+        const secondCount = counts.get(second)
+        if(secondCount!== undefined){
+            counts.set(second , secondCount+amount)
+        } else{
+            counts.set(second, amount)
+        }
+    }
+
+    for(const [k, v] of counts){
+        counts.set(k, Math.ceil(v))
+    }
 
     return getAnswer(counts)
 }
